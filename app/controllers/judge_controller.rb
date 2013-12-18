474c9115c7e02a473f79ed0406ad30747f235457
judger <judger@judgvalue.com>
2013-12-18 23:57:34 +0900

    search basic

--- app/controllers/judge_controller.rb
+++ app/controllers/judge_controller.rb
@@ -6,13 +6,12 @@ class JudgeController < ApplicationController
 
 
   def buffett
-    stock = Stock.find_by(code: params[:code])
+    @stock = Stock.find_by(code: params[:code])
 
-    if stock.nil?
+    if @stock.nil?
       flash[:notice] = "stock nonexist! code=#{params[:code]}"
       redirect_to action: :index
     end
-
 
   end
 end
