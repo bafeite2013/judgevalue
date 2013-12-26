510461569b66e51779b0595ff04416da50fd4d43
judger <judger@judgvalue.com>
2013-12-27 01:38:45 +0900

    buffett updated, made a good function for display line diagrams

--- app/assets/javascripts/judge.js
+++ app/assets/javascripts/judge.js
@@ -15,182 +15,60 @@ judgeApp.factory("buffett", ['$resource', function($resource) {
   return $resource('/judge/buffett.json');
 }]);
 
-judgeApp.controller("EarningsPerShareController", ["$scope", "$http", "buffett", function ($scope, $http, buffett) {
-  $scope.init = function(stock_id, columns) {
-    $scope.stock_id = stock_id;
+judgeApp.controller("LineDiagramController", ["$scope", "$http", "buffett", function ($scope, $http, buffett) {
+  $scope.init = function(stock_id, name, columns, replace_id) {
+    buffett.query({id: stock_id}, function(indices) {
+      var year_array = [];
+      var series_array = [];
+      var key, index, num, counter;
+      for (key in columns) {
+        series_array.push({id: key, name: columns[key], data: []});
+      }
 
-    var options={
-      chart: { type: 'line' },
-      title: { text: '' },
-      xAxis: {
-        categories: [],
-        lineWidth: 2
-      },
-      yAxis: {
-        title:{
+      for (index = 0; index < indices.length; index += 1) {
+        year_array[index] = indices[index].year;
+        counter = 0;
+        for (key in columns) {
+          num = Number(indices[index][key]);
+          series_array[counter].data.push(isNaN(num)?null:num);
+          counter += 1;
+        }
+      }
+
+      var options = {
+        chart: { type: 'line' },
+        title: { text: name },
+        xAxis: {
+          categories: year_array,
+          lineWidth: 2
+        },
+        yAxis: {
+          title:{
+            enabled:false
+          },
+          lineWidth: 2
+        },
+        tooltip: {
+          formatter: function() {
+            var s = '<b>'+ this.x +'</b>';
+            $.each(this.points, function(i, point) {
+              s += '<br/>'+ point.series.name +': '+point.y;
+            });
+            return s;
+          }, shared: true
+        },
+        exporting: {
           enabled:false
         },
-        lineWidth: 2
-      },
-      tooltip: {
-        formatter: function() {
-          var s = '<b>'+ this.x +'</b>';
-          $.each(this.points, function(i, point) {
-            s += '<br/>'+ point.series.name +': '+point.y;
-          });
-          return s;
-        }, shared: true
-      },
-      exporting: {
-        enabled:false
-      },
-      credits:{
-        text: 'judgvalue.com',
-        href:'http://judgvalue.com'
-      },
-      series: [
-        {
-          id:'earnings_per_share',
-          name: 'Earnings Per Share',
-          data: []
-        }
-      ]
-    };
+        credits:{
+          text: 'judgvalue.com',
+          href:'http://judgvalue.com'
+        },
+        series: series_array
+      };
 
-    indices = buffett.query({id: stock_id}, function() {
-      var year_array = [];
-      var data_array = [];
-      for (var index in indices) {
-        year_array[index] = indices[index].year;
-        data_array[index] = Number(indices[index].earnings_per_share);
-      }
-      options.xAxis.categories=year_array;
-      $('#chart-earnings-per-share').highcharts(options);
-      var chart = $('#chart-earnings-per-share').highcharts();
-      chart.get('earnings_per_share').setData(data_array);
+      $(replace_id).highcharts(options);
     });
   };
 
 }]);
-
-judgeApp.controller("NetAssetsPerShareController", function ($scope, $http) {
-  $scope.init = function(stock_id) {
-    $scope.stock_id = stock_id;
-
-    var options={
-      chart: { type: 'line' },
-      title: { text: '每股净资产历史变化图' },
-      xAxis: {
-        categories: [],
-        lineWidth: 2
-      },
-      yAxis: {
-        title:{
-          enabled:false
-        },
-        lineWidth: 2
-      },
-      tooltip: {
-        formatter: function() {
-          var s = '<b>'+ this.x +'</b>';
-          $.each(this.points, function(i, point) {
-            s += '<br/>'+ point.series.name +': '+point.y;
-          });
-          return s;
-        }, shared: true
-      },
-      exporting: {
-        enabled:false
-      },
-      credits:{
-        text: 'judgvalue.com',
-        href:'http://judgvalue.com'
-      },
-      series: [
-        {
-          id:'net_assets_per_share',
-          name: 'Net Assets Per Share',
-          data: []
-        }
-      ]
-    };
-
-    $http.get("/judge/net_assets_per_share.json?id="+$scope.stock_id).success(
-      function(data, status, headers, config) {
-        var year_array = [];
-        var data_array = [];
-        for (var index in data) {
-          year_array[index] = data[index].year;
-          data_array[index] = Number(data[index].net_assets_per_share);
-        }
-        options.xAxis.categories=year_array;
-        $('#chart-net-assets-per-share').highcharts(options);
-        var chart = $('#chart-net-assets-per-share').highcharts();
-        chart.get('net_assets_per_share').setData(data_array);
-      }
-    );
-
-  };
-
-});
-
-judgeApp.controller("ReturnOnEquityController", function ($scope, $http) {
-  $scope.init = function(stock_id) {
-    $scope.stock_id = stock_id;
-
-    var options={
-      chart: { type: 'line' },
-      title: { text: '净资产收益率历史变化图' },
-      xAxis: {
-        categories: [],
-        lineWidth: 2
-      },
-      yAxis: {
-        title:{
-          enabled:false
-        },
-        lineWidth: 2
-      },
-      tooltip: {
-        formatter: function() {
-          var s = '<b>'+ this.x +'</b>';
-          $.each(this.points, function(i, point) {
-            s += '<br/>'+ point.series.name +': '+point.y;
-          });
-          return s;
-        }, shared: true
-      },
-      exporting: {
-        enabled:false
-      },
-      credits:{
-        text: 'judgvalue.com',
-        href:'http://judgvalue.com'
-      },
-      series: [
-        {
-          id:'return_on_equity',
-          name: 'Return On Equity',
-          data: []
-        }
-      ]
-    };
-
-    $http.get("/judge/return_on_equity.json?id="+$scope.stock_id).success(
-      function(data, status, headers, config) {
-        var year_array = [];
-        var data_array = [];
-        for (var index in data) {
-          year_array[index] = data[index].year;
-          data_array[index] = Number(data[index].return_on_equity);
-        }
-        options.xAxis.categories=year_array;
-        $('#chart-return-on-equity').highcharts(options);
-        var chart = $('#chart-return-on-equity').highcharts();
-        chart.get('return_on_equity').setData(data_array);
-      }
-    );
-
-  };
-
-});
